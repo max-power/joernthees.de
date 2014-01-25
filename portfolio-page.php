a3bf44b421ca2e79d89df546ee65bd1499483f09
Kevin <kevin.melchert@gmail.com>
2014-01-25 17:56:28 +0100

    head and feet from CMS

--- portfolio-page.php
+++ portfolio-page.php
@@ -1,10 +1,11 @@
+<?php $homepage = $pages->get('/') ?>
 <section class="works" id="<?php echo $page->name; ?>">
   <div class="wrap">
-    <header class="joern"></header>
+    <header class="joern" style="background-image:url(<?php echo $homepage->joern_head->first->url; ?>)"></header>
     <div id="tv" class="tv">
       <?php foreach($page->children as $child) { echo $child->render(); } ?>
     </div>
-    <footer class="joern"></footer>
+    <footer class="joern" style="background-image:url(<?php echo $homepage->joern_feet->first->url; ?>)"></footer>
   </div>
 </section>
 
