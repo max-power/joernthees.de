13e169a5d9e187fcd3afd3b4b6bce0f940d2c801
Kevin <kevin.melchert@gmail.com>
2014-01-25 18:59:18 +0100

    better static noise

--- portfolio-page.php
+++ portfolio-page.php
@@ -3,7 +3,9 @@
   <div class="wrap">
     <header class="joern" style="background-image:url(<?php echo $homepage->joern_head->first->url; ?>)"></header>
     <div id="tv" class="tv">
-      <?php foreach($page->children as $child) { echo $child->render(); } ?>
+      <div class="tube">
+        <?php foreach($page->children as $child) { echo $child->render(); } ?>
+      </div>
     </div>
     <footer class="joern" style="background-image:url(<?php echo $homepage->joern_feet->first->url; ?>)"></footer>
   </div>
